315e4e6c0a42d013491ec4b8ae5963cf2896ccdf
Ali Hassan <malikalihassankhan786@gmail.com>
2025-12-09 14:21:32 +0500

    Added Organization and Products details

--- homie_app/homie_app/doctype/animal_shelter_partners/animal_shelter_partners.js
+++ homie_app/homie_app/doctype/animal_shelter_partners/animal_shelter_partners.js
@@ -1,7 +1,7 @@
 // Copyright (c) 2025, Anonymous and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('Animal Shelter Patners', {
+frappe.ui.form.on('Animal Shelter Partners', {
 	// refresh: function(frm) {
 
 	// }
--- homie_app/homie_app/doctype/animal_shelter_partners/test_animal_shelter_partners.py
+++ homie_app/homie_app/doctype/animal_shelter_partners/test_animal_shelter_partners.py
@@ -5,5 +5,5 @@
 from frappe.tests.utils import FrappeTestCase
 
 
-class TestAnimalShelterPatners(FrappeTestCase):
+class TestAnimalShelterPartners(FrappeTestCase):
 	pass
