52d031b1de8bd424939a3e551f09f4a76d0f28a3
Ali Hassan <malikalihassankhan786@gmail.com>
2025-12-09 19:22:30 +0500

    Adjusted doctype association person field

--- homie_app/homie_app/doctype/deleivery_informations/deleivery_informationss.py
+++
@@ -1,8 +0,0 @@
-# Copyright (c) 2025, Anonymous and contributors
-# For license information, please see license.txt
-
-# import frappe
-from frappe.model.document import Document
-
-class DeleiveryInformations(Document):
-	pass
