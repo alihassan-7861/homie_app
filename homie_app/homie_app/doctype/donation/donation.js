eb6140e5866349117d47b024ebca4605ad719bbd
Ali Hassan <malikalihassankhan786@gmail.com>
2025-12-12 15:40:46 +0500

    Added Fields in Donations

--- homie_app/homie_app/doctype/donation/donation.js
+++ homie_app/homie_app/doctype/donation/donation.js
@@ -36,3 +36,32 @@ function update_parent_total(frm) {
 
 
 
+frappe.ui.form.on('Donation', {
+    contact_person: function(frm) {
+        if (frm.doc.contact_person) {
+            frappe.db.get_doc('Association Contact Person info', frm.doc.contact_person)
+                .then(doc => {
+                    frm.set_value('person_first_name', doc.first_name);
+                    frm.set_value('person_last_name', doc.last_name);
+                    frm.set_value('person_email', doc.email);
+                });
+        } else {
+            frm.set_value('person_first_name', '');
+            frm.set_value('person_last_name', '');
+            frm.set_value('person_email', '');
+        }
+    },
+
+    organization: function(frm) {
+        if (frm.doc.organization) {
+            frappe.db.get_doc('Organization Details', frm.doc.organization)
+                .then(doc => {
+                    frm.set_value('organization_name', doc.organization_name);
+                });
+        } else {
+            frm.set_value('organization_name', '');
+        }
+    }
+});
+
+
